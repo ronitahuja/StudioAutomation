5e624a72aee1a772547f08e0725709ed14788be2
Harsh9307 <diwaseharsh@gmail.com>
2025-03-05 14:47:39 +0530

    created apis for fetching appcategory and authenticationTypes

--- backend/src/controllers/app.controller.js
+++ backend/src/controllers/app.controller.js
@@ -8,7 +8,7 @@ const appService = new AppService(new AppRepository());
 async function pingAppController(req,res){
     return res.json({message:"App controller is up"})
 }
-async function createApp(req,res){
+async function createApp(req,res,next){
     try{
         const app = await appService.createApp(req.body);
         return res.status(StatusCodes.OK).json({
@@ -23,7 +23,40 @@ async function createApp(req,res){
     }
 }
 
+async function getAppCategory(req,res,next){
+    try{
+        const appCategories = await appService.getAppCategory();
+        return res.status(StatusCodes.OK).json({
+            success:true,
+            message:"succesfully fetched app categories",
+            error:{},
+            data : appCategories
+        })
+    }
+    catch(err){
+        next(err);
+    }
+}
+
+async function getAuthenticationType(req,res,next){
+    try{
+        const authenticationTypes = await appService.getAuthenticationType();
+        return res.status(StatusCodes.OK).json({
+            success:true,
+            message:"succesfully fetched authenticationTypes",
+            error:{},
+            data : authenticationTypes
+        })
+    }
+    catch(err){
+        next(err);
+    }
+}
+
+
 module.exports ={
     pingAppController,
-    createApp
+    createApp,
+    getAppCategory,
+    getAuthenticationType
 }
--- backend/src/services/app.service.js
+++ backend/src/services/app.service.js
@@ -6,5 +6,13 @@ class AppService{
         const app = await this.appRepository.createApp(appData);
         return app;
     }
+    async getAppCategory(){
+        const appCategories =  await this.appRepository.getAppCategory();
+        return appCategories;
+    }
+    async getAuthenticationType(){
+        const authenticationTypes = await this.appRepository.getAuthenticationType();
+        return authenticationTypes;
+    }
 }
 module.exports = AppService;
