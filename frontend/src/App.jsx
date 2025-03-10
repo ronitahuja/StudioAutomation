c3ae41845480e72bc3e6a1041826e0f55cb5bd60
Harsh9307 <diwaseharsh@gmail.com>
2025-03-10 14:34:51 +0530

    added the application table

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,12 @@ import Form from './components/Form'
 import {Routes,Route} from 'react-router-dom'
 import AppForm from './components/AppForm'
 
+
 function App() {
   return (
     <>
       <Header />
+      
       <Routes>
         <Route
           path="/"
