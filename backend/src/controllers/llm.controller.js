97ec26e72b765fe4184939643ae91845b0014021
ronitdarwinbox <ahujaronit31@gmail.com>
2025-03-10 13:58:28 +0530

    added new ai models and dockerfile

--- backend/src/controllers/llm.controller.js
+++ backend/src/controllers/llm.controller.js
@@ -8,16 +8,6 @@ class LLMController {
     try {
       const queryObj = req.body;
       const response = await this.llmService.queryLLM(queryObj);
-      // const evaluatedResponse= await this.llmService.checkResponse(queryObj, response);
-
-      // uncomment this if u want to enable response chaining
-
-      // return res.status(200).json({
-      //   success: true,
-      //   message: "successfully queried",
-      //   error: {},
-      //   data: evaluatedResponse,
-      // });
 
       return res.status(200).json({
         success: true,
--- frontend/src/constants/models.js
+++ frontend/src/constants/models.js
@@ -1,18 +1,93 @@
-import llama from '../assets/llama.png';
-import deepseek from '../assets/deepseek.png';
+import {
+  mistral,
+  huggingface,
+  google,
+  meta,
+  llama,
+  qwen,
+  deepseek,
+} from "../assets";
 
 const models = [
-    {
-        name:"llama3.1:8b",
-        image:llama
-    }, 
-    {
-        name:"deepseek-r1:1.5b",
-        image:deepseek
-    }, 
-    {
-        name:"codellama",
-        image:llama
-    }
+  {
+    name: "distil-whisper-large-v3-en",
+    image: huggingface,
+  },
+  {
+    name: "whisper-large-v3",
+    image: huggingface,
+  },
+  {
+    name: "whisper-large-v3-turbo",
+    image: huggingface,
+  },
+  {
+    name: "gemma2-9b-it",
+    image: google,
+  },
+  {
+    name: "mixtral-8x7b-32768",
+    image: mistral,
+  },
+  {
+    name: "mistral-saba-24b",
+    image: mistral,
+  },
+  {
+    name: "qwen-qwq-32b",
+    image: qwen,
+  },
+  {
+    name: "qwen-2.5-coder-32b",
+    image: qwen,
+  },
+  {
+    name: "qwen-2.5-32b",
+    image: qwen,
+  },
+  {
+    name: "deepseek-r1-distill-qwen-32b",
+    image: deepseek,
+  },
+  {
+    name: "deepseek-r1-distill-llama-70b-specdec",
+    image: deepseek,
+  },
+  {
+    name: "deepseek-r1-distill-llama-70b",
+    image: deepseek,
+  },
+  {
+    name: "llama-3.3-70b-versatile",
+    image: meta,
+  },
+  {
+    name: "llama-3.1-8b-instant",
+    image: meta,
+  },
+  {
+    name: "llama-guard-3-8b",
+    image: meta,
+  },
+  {
+    name: "llama3-70b-8192",
+    image: meta,
+  },
+  {
+    name: "llama3-8b-8192",
+    image: meta,
+  },
+  {
+    name: "llama-3.3-70b-specdec",
+    image: meta,
+  },
+  {
+    name: "llama-3.2-1b-preview",
+    image: meta,
+  },
+  {
+    name: "llama-3.2-3b-preview",
+    image: meta,
+  },
 ];
 export default models;
