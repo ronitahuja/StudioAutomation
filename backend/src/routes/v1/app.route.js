919af9b8a8782a14edc0fc4f30d73cd1998bca76
Harsh9307 <diwaseharsh@gmail.com>
2025-03-12 11:14:35 +0530

    added the edit functionality

--- backend/src/routes/v1/app.route.js
+++ backend/src/routes/v1/app.route.js
@@ -10,8 +10,8 @@ AppRouter.get('/appCategories',AppController.getAppCategory);
 AppRouter.get('/authenticationType',AppController.getAuthenticationType)
 AppRouter.get('/appNames',AppController.getAppNames);
 AppRouter.get('/appnames/:appName',AppController.getConnectionLevelParams);
-AppRouter.put('/appnames/:appName',AppController.updateApp);
 AppRouter.delete('/appnames/:appName',AppController.deleteApp);
+AppRouter.patch('/:appName',AppController.updateApp);
 AppRouter.get('/:appName',AppController.getApp);
 AppRouter.post('/',AppController.createApp);
 AppRouter.get('/',AppController.getAllApps);
