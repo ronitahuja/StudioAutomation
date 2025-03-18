1c367772e48584e1e660ed409573d0d88919518f
Harsh9307 <diwaseharsh@gmail.com>
2025-03-18 11:12:59 +0530

    small bug in index.js of Service layer

--- backend/src/services/index.js
+++ backend/src/services/index.js
@@ -1,6 +1,6 @@
 module.exports={
     FunctionService : require('./function.service'),
-    LlLMervice : require('./llm.service'),
+    LLMService : require('./llm.service'),
     AppActionService : require('./appAction.service'),
     AppService : require('./app.service'),
     AnalysisService:require('./analysis.service')
